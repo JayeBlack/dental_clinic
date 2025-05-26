dcd4cfbdc4f50c741fc589f5f0ae07c7e159aea3
jayeblack <jayeblack19@gmail.com>
2025-05-26 01:36:51 +0000

    modified permissions file

--- backend/apps/appointments/permissions.py
+++ backend/apps/appointments/permissions.py
@@ -1,6 +1,7 @@
 from rest_framework import permissions
 from rest_framework.generics import RetrieveAPIView
 
+
 class AppointmentPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         if not request.user.is_authenticated:
